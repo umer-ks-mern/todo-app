59d3bf0a3d09c717ba1e0216644c6b426f6c57c8
Umer <aehtesham086@gmail.com>
2023-09-02 17:22:51 +0500

    added the local storage functionality, data does not vanish upon reload/refresh

--- src/pages/todo/TodoWrapper.jsx
+++ src/pages/todo/TodoWrapper.jsx
@@ -1,16 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TodoAdd from "../../components/todo/TodoAdd";
 import TodoList from "../../components/todo/TodoList";
 
 const TodoWrapper = () => {
   const [todos, setTodos] = useState([]);
 
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem("todos"));
+    if (storedTodos) {
+      setTodos(storedTodos);
+    }
+  }, []);
+
   const addTodo = (newTodo) => {
-    setTodos([...todos, newTodo]);
+    // Update the state with the newTodo
+    const updatedTodos = [...todos, newTodo];
+    setTodos(updatedTodos);
+    
+    // Save the updatedTodos to local storage
+    localStorage.setItem("todos", JSON.stringify(updatedTodos));
+  };
+  
+  const deleteHistory = () => {
+    localStorage.setItem("todos", JSON.stringify([]));
+    setTodos([]);
+    alert("History Cleared!");
   };
 
   return (
     <>
+      <button onClick={deleteHistory}>Delete History</button>
       <TodoAdd onAddTodo={addTodo} />
       <TodoList todos={todos} />
     </>
--- src/readme.txt
+++ src/readme.txt
@@ -3,5 +3,3 @@ use local storage of js
 localStorage.setItems("name", arrayOfObjects)
 
 localStorage.getItems("name", arrayOfObjects)
-
-@babel/plugin-proposal-private-property-in-object
